e5c5ff2600114d83f2b53e1386b8b91a1fc453bd
jigar <jigar1.dev@gmail.com>
2026-08-20 12:55:48 +0530

    route issue resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { getCompletedPortfolioUsername } from "@/lib/portfolioSetup";
-import { SESSION_COOKIE, verifySessionToken } from "@/lib/session";
 import LandingPage from "./components/landing/LandingPage";
 
-export default async function Home() {
-  const cookieStore = await cookies();
-  const session = await verifySessionToken(cookieStore.get(SESSION_COOKIE)?.value);
-  const username = session && await getCompletedPortfolioUsername(session.userId);
-  if (username) redirect(`/${encodeURIComponent(username)}`);
-
+export default function Home() {
   return <LandingPage />;
 }
